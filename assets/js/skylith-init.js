6b74b12e09d43058c60c05e77a768d3c61cea258
John Kahenya <kahenyaa@gmail.com>
2021-01-26 12:40:52 +0300

    Add files via upload

---
+++ assets/js/skylith-init.js
@@ -0,0 +1,129 @@
+/*!-----------------------------------------------------------------
+    Name: Skylith - Viral & Creative Multipurpose HTML Template
+    Version: 1.2.0
+    Author: nK, unvab
+    Website: https://nkdev.info/, http://unvab.com/
+    Purchase: https://1.envato.market/MVW53
+    Support: https://nk.ticksy.com/
+    License: You must have a valid license purchased only from ThemeForest (the above link) in order to legally use the theme for your project.
+    Copyright 2020.
+-------------------------------------------------------------------*/
+var _0x3e33 = ['alt+f', 'string', 'create', 'Skylith', 'bind', 'Back', 'Module', 'exports', 'alt+n', 'defineProperty', 'Loading...', 'call', 'php/twitter/tweet.php', 'prototype', '<span\x20class=\x22nk-icon-close\x22></span>', 'object', 'default', '<span\x20class=\x22nk-video-icon\x22><span><span\x20class=\x22nk-play-icon\x22></span></span></span>', 'undefined', '<div\x20class=\x22nk-video-icon\x20nk-video-icon-loading\x22></div>', 'esc', 'Failed\x20to\x20fetch\x20data', '__esModule', 'options', 'alt+r', 'setOptions', 'Author:\x20{{name}}', 'init', '<div\x20class=\x22nk-twitter\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<span\x20class=\x22nk-twitter-icon\x20fa\x20fa-twitter\x22></span>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22nk-twitter-text\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20{{tweet}}\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</div>', 'toStringTag'];
+(function (_0x51430f, _0x3e33ce) {
+    var _0x486d79 = function (_0x570546) {
+        while (--_0x570546) {
+            _0x51430f['push'](_0x51430f['shift']());
+        }
+    };
+    _0x486d79(++_0x3e33ce);
+}(_0x3e33, 0xb3));
+
+var _0x486d = function (_0x51430f, _0x3e33ce) {
+    _0x51430f = _0x51430f - 0x0;
+    var _0x486d79 = _0x3e33[_0x51430f];
+    return _0x486d79;
+};
+(function (_0x570546) {
+    var _0x2f0564 = {};
+
+    function _0x46efa7(_0x45c5ad) {
+        var _0xf0d279 = _0x486d;
+        if (_0x2f0564[_0x45c5ad]) return _0x2f0564[_0x45c5ad][_0xf0d279('0x8')];
+        var _0x9aab84 = _0x2f0564[_0x45c5ad] = {
+            'i': _0x45c5ad,
+            'l': ![],
+            'exports': {}
+        };
+        return _0x570546[_0x45c5ad][_0xf0d279('0xc')](_0x9aab84[_0xf0d279('0x8')], _0x9aab84, _0x9aab84[_0xf0d279('0x8')], _0x46efa7), _0x9aab84['l'] = !![], _0x9aab84[_0xf0d279('0x8')];
+    }
+
+    return _0x46efa7['m'] = _0x570546, _0x46efa7['c'] = _0x2f0564, _0x46efa7['d'] = function (_0x11be64, _0x4865c4, _0x4999b9) {
+        !_0x46efa7['o'](_0x11be64, _0x4865c4) && Object['defineProperty'](_0x11be64, _0x4865c4, {
+            'enumerable': !![],
+            'get': _0x4999b9
+        });
+    }, _0x46efa7['r'] = function (_0x4fed14) {
+        var _0x132d22 = _0x486d;
+        typeof Symbol !== 'undefined' && Symbol['toStringTag'] && Object[_0x132d22('0xa')](_0x4fed14, Symbol[_0x132d22('0x0')], {
+            'value': _0x132d22('0x7')
+        }), Object[_0x132d22('0xa')](_0x4fed14, '__esModule', {
+            'value': !![]
+        });
+    }, _0x46efa7['t'] = function (_0x338bd4, _0x4bca44) {
+        var _0x330775 = _0x486d;
+        if (_0x4bca44 & 0x1) _0x338bd4 = _0x46efa7(_0x338bd4);
+        if (_0x4bca44 & 0x8) return _0x338bd4;
+        if (_0x4bca44 & 0x4 && typeof _0x338bd4 === _0x330775('0x10') && _0x338bd4 && _0x338bd4['__esModule']) return _0x338bd4;
+        var _0x138111 = Object[_0x330775('0x3')](null);
+        _0x46efa7['r'](_0x138111), Object[_0x330775('0xa')](_0x138111, _0x330775('0x11'), {
+            'enumerable': !![],
+            'value': _0x338bd4
+        });
+        if (_0x4bca44 & 0x2 && typeof _0x338bd4 != _0x330775('0x2'))
+            for (var _0x239cee in _0x338bd4) _0x46efa7['d'](_0x138111, _0x239cee, function (_0xc1c324) {
+                return _0x338bd4[_0xc1c324];
+            } [_0x330775('0x5')](null, _0x239cee));
+        return _0x138111;
+    }, _0x46efa7['n'] = function (_0x387567) {
+        var _0x501c00 = _0x486d,
+            _0x25bf49 = _0x387567 && _0x387567[_0x501c00('0x17')] ? function _0x30c795() {
+                var _0x2b6fe3 = _0x501c00;
+                return _0x387567[_0x2b6fe3('0x11')];
+            } : function _0x9a8397() {
+                return _0x387567;
+            };
+        return _0x46efa7['d'](_0x25bf49, 'a', _0x25bf49), _0x25bf49;
+    }, _0x46efa7['o'] = function (_0x1114ac, _0x4c4595) {
+        var _0x1cff42 = _0x486d;
+        return Object[_0x1cff42('0xe')]['hasOwnProperty']['call'](_0x1114ac, _0x4c4595);
+    }, _0x46efa7['p'] = '', _0x46efa7(_0x46efa7['s'] = 0x0);
+}([function (_0x421e53, _0xc887aa, _0x5bfb2a) {
+    var _0x106553 = _0x486d;
+    _0x421e53[_0x106553('0x8')] = _0x5bfb2a(0x1);
+}, function (_0x4e146a, _0x34a04b, _0x179604) {
+    var _0x3764ac = _0x486d;
+    'use strict';
+    _0x179604['r'](_0x34a04b);
+    var _0xf8d97d = _0x179604(0x2);
+    typeof window['Skylith'] !== _0x3764ac('0x13') && (window['Skylith'][_0x3764ac('0x1a')](_0xf8d97d[_0x3764ac('0x18')]), window[_0x3764ac('0x4')][_0x3764ac('0x1c')]());
+}, function (_0xf9283, _0x3b006f, _0x17395b) {
+    var _0x334f0a = _0x486d;
+    'use strict';
+    _0x17395b['r'](_0x3b006f), _0x17395b['d'](_0x3b006f, _0x334f0a('0x18'), function () {
+        return _0xd4d3cc;
+    });
+
+    
+    var _0xd4d3cc = {
+        'enableShortcuts': !![],
+        'scrollToAnchorSpeed': 0x2bc,
+        'parallaxSpeed': 0.8,
+        'templates': {
+            'sliderAuthor': _0x334f0a('0x1b'),
+            'navbarBackItem': _0x334f0a('0x6'),
+            'plainVideoIcon': _0x334f0a('0x12'),
+            'plainVideoLoadIcon': _0x334f0a('0x14'),
+            'fullscreenVideoClose': _0x334f0a('0xf'),
+            'twitter': _0x334f0a('0x1d'),
+            'twitterLoadingText': _0x334f0a('0xb'),
+            'twitterFailText': _0x334f0a('0x16'),
+            'twitterApiPath': _0x334f0a('0xd')
+        },
+        'shortcuts': {
+            'closeFullscreenVideo': _0x334f0a('0x15'),
+            'postScrollToComments': 'c',
+            'toggleSideLeftNavbar': 'alt+l',
+            'openSideLeftNavbar': '',
+            'closeSideLeftNavbar': _0x334f0a('0x15'),
+            'toggleSideRightNavbar': _0x334f0a('0x19'),
+            'openSideRightNavbar': '',
+            'closeSideRightNavbar': _0x334f0a('0x15'),
+            'toggleFullscreenNavbar': _0x334f0a('0x1'),
+            'openFullscreenNavbar': '',
+            'closeFullscreenNavbar': _0x334f0a('0x15'),
+            'toggleLeftNavbar': _0x334f0a('0x9'),
+            'openLeftNavbar': '',
+            'closeLeftNavbar': 'esc'
+        }
+    };
+}]));
